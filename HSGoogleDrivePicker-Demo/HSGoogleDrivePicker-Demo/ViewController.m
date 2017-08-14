d1cd14942b38b191f7ce43e2efb44a8e2e7deb04
Rob Jonson <rob@hobbyistsoftware.com>
2017-08-14 15:21:31 +0100

    demo version

---
+++ HSGoogleDrivePicker-Demo/HSGoogleDrivePicker-Demo/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  HSGoogleDrivePicker-Demo
+//
+//  Created by Rob Jonson on 14/08/2017.
+//  Copyright © 2017 HobbyistSoftware. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "HSDrivePicker.h"
+
+//REQUIRED STEPS TO RUN DEMO
+// 1) Provide your secret below
+// 2) Add your 'GoogleService-Info.plist' plist to the project
+// 3) Configure the correct callback URL
+//
+// See documentation for full details: https://github.com/ConfusedVorlon/HSGoogleDrivePicker#getting-your-api-keys
+
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pickFile:(id)sender {
+    
+    HSDrivePicker *picker=[[HSDrivePicker alloc] initWithSecret:@"YOUR SECRET HERE"];
+    
+    
+    [picker pickFromViewController:self
+                    withCompletion:^(HSDriveManager *manager, GTLDriveFile *file) {
+                        NSLog(@"selected: %@",file.name);
+                    }];
+}
+
+
+
+@end
